91b53a03a4c230e54ac72a94cc2a5d2a3a5c745e
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-01-23 20:32:14 +0100

    introduce threadinfo(); fix cpuids starting with 0

--- src/api.jl
+++ src/api.jl
@@ -9,7 +9,7 @@ getcpuid() = Int(sched_getcpu())
 
 """
 Returns the ID of the CPUs on which the Julia threads
-are currently running. The result is ordered.
+are currently running.
 
 See `getcpuid` for more information.
 """
@@ -69,19 +69,64 @@ function pinthreads(strategy::Symbol; nthreads = Threads.nthreads(), warn::Bool 
     end
 end
 
-_pin_compact(nthreads) = pinthreads(1:nthreads; warn = false)
+_pin_compact(nthreads) = pinthreads(0:nthreads-1; warn = false)
 function _pin_scatter(nthreads; hyperthreading = false, nsockets = 2, verbose = false, kwargs...)
     verbose && @info("Assuming $nsockets sockets and the ", hyperthreading ? "availability" : "absence", " of hyperthreads.")
     ncpus = Sys.CPU_THREADS
     if !hyperthreading
-        cpuids_per_socket = Iterators.partition(1:ncpus, ncpus ÷ nsockets)
+        cpuids_per_socket = Iterators.partition(0:ncpus-1, ncpus ÷ nsockets)
         cpuids = interweave(cpuids_per_socket...)
     else
         # alternate between sockets but use hyperthreads (i.e. 2 threads per core) only if necessary
-        cpuids_per_socket_and_hyper = collect.(Iterators.partition(1:ncpus, (ncpus ÷ 2) ÷ nsockets))
+        cpuids_per_socket_and_hyper = collect.(Iterators.partition(0:ncpus-1, (ncpus ÷ 2) ÷ nsockets))
         cpuids_per_socket = [reduce(vcat, cpuids_per_socket_and_hyper[s:nsockets:end]) for s in 1:nsockets]
         cpuids = interweave(cpuids_per_socket...)
     end
     pinthreads(@view cpuids[1:nthreads]; warn = false)
+    return nothing
+end
+
+"""
+Print information about Julia threads, e.g. on which CPU-cores they are running.
+
+By default, the visualization will be based on `Sys.CPU_THREADS` only.
+If you also load Hwloc.jl (via `using Hwloc`) it will show more detailed information.
+"""
+function threadinfo()
+    # general info
+    thread_cpuids = getcpuids()
+    # visualize current pinning
+    println()
+    println("Julia threads: ", Threads.nthreads())
+    println("Occupied cores: ", length(unique(thread_cpuids)), "\n")
+    _visualize_affinity(; thread_cpuids)
+    println("Thread-Core mapping:")
+    for (tid, core) in enumerate(thread_cpuids)
+        println("  $tid => $core")
+        if tid == 10
+            println("  ...")
+            break
+        end
+    end
+    println()
+    return nothing
+end
+
+function _visualize_affinity(; thread_cpuids = getcpuids())
+    print(" ")
+    for puid in 0:Sys.CPU_THREADS-1
+        if puid in thread_cpuids
+            printstyled(puid, bold = true, underline = false, color = :red)
+        else
+            print(puid)
+        end
+        !(puid == Sys.CPU_THREADS - 1) && print(",")
+    end
+    println()
+    # legend
+    println()
+    printstyled("#", bold = true, underline = false, color = :red)
+    print(" = Julia thread")
+    println("\n")
     return nothing
 end
