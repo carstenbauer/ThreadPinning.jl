d8475679d068f23f2774c7f0aff99b6d4b85f68f
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-03-23 13:04:34 +0100

    sysinfo printing

--- src/ThreadPinning.jl
+++ src/ThreadPinning.jl
@@ -16,6 +16,21 @@ Base.@kwdef struct SysInfo
     cpuids_numa::Vector{Vector{Int}} = [collect(0:(Sys.CPU_THREADS - 1))]
     ishyperthread::Vector{Bool} = fill(false, Sys.CPU_THREADS)
 end
+function Base.show(io::IO, sysinfo::SysInfo)
+    return print(io, "SysInfo()")
+end
+function Base.show(io::IO, mime::MIME{Symbol("text/plain")}, sysinfo::SysInfo)
+    summary(io, sysinfo)
+    println(io)
+    fnames = fieldnames(SysInfo)
+    for fname in fnames[1:(end - 1)]
+        println(io, "├ $fname: ", getfield(sysinfo, fname))
+    end
+    print(io, "└ $(fnames[end]): ", getfield(sysinfo, fnames[end]))
+    return nothing
+end
+
+# constants
 const SYSINFO_ATTEMPT = Ref{Bool}(false) # have we yet attempted to gather the sysinfo
 const SYSINFO_SUCCESS = Ref{Bool}(false) # have we succeeded in gathering the sysinfo yet
 const SYSINFO = Ref{SysInfo}(SysInfo())
