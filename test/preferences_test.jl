e1ea248f3e89f90f579a87baca2ce70fac6c3180
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-10-09 11:29:48 +0200

    autoupdate pref tests

--- test/preferences_test.jl
+++ test/preferences_test.jl
@@ -11,9 +11,17 @@ using Test
     @test_throws ArgumentError ThreadPinning.Prefs.set_pinning(:asd)
     @test_throws ArgumentError ThreadPinning.Prefs.set_places(:asd)
 
+    @test isnothing(ThreadPinning.Prefs.set_autoupdate(false))
+    @test ThreadPinning.Prefs.get_autoupdate() == false
+    @test isnothing(ThreadPinning.Prefs.set_autoupdate(true))
+
+    @test isnothing(ThreadPinning.Prefs.showall())
+
     @test ThreadPinning.Prefs.has_pinning()
     @test ThreadPinning.Prefs.has_places()
+    @test ThreadPinning.Prefs.has_autoupdate()
     @test isnothing(ThreadPinning.Prefs.clear())
     @test !ThreadPinning.Prefs.has_pinning()
     @test !ThreadPinning.Prefs.has_places()
+    @test !ThreadPinning.Prefs.has_autoupdate()
 end
