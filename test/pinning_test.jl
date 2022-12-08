463389ac9b0130e945624ea7ebc40b0ad7dfed40
JamesWrigley <james@puiterwijk.org>
2022-12-08 15:58:17 +0100

    Reimplement the :firstn strategy

--- test/pinning_test.jl
+++ test/pinning_test.jl
@@ -143,3 +143,14 @@ end
     # Check that at least some of the pinning settings were different
     @test any(x != cpuids[1] for x in cpuids)
 end
+
+@testset "Thread Pinning (firstn)" begin
+    expected_pinning = cpuids_all()[1:nthreads()]
+
+    # Make sure we have a different pinning to start with
+    pinthreads(reverse(expected_pinning))
+
+    @test getcpuids() != expected_pinning
+    pinthreads(:firstn)
+    @test getcpuids() == expected_pinning
+end
