54ed1143ef7a1dfc55f72145a59d5b855dc2aa64
Carsten Bauer <crstnbr@googlemail.com>
2022-12-15 12:05:57 +0100

    systems_test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,10 @@ Threads.nthreads() ≥ 2 ||
 @run_package_tests
 
 @testitem "Helper" begin include("helper_test.jl") end
-@testitem "System Info" begin include("sysinfo_test.jl") end
 @testitem "Pinning" begin include("pinning_test.jl") end
 @testitem "Querying" begin include("querying_test.jl") end
 @testitem "threadinfo" begin include("threadinfo_test.jl") end
 @testitem "OMP" begin include("omp_test.jl") end
 @testitem "Preferences" begin include("preferences_test.jl") end
+@testitem "Systems Info" begin include("systems_test.jl") end
 @testitem "likwid-pin" begin include("likwid-pin_test.jl") end
