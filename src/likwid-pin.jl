7e9e082d901c47a63117a2bb59e06b9f077d5a56
Carsten Bauer <crstnbr@gmail.com>
2022-12-18 21:13:24 +0100

    likwidpin: onebased

--- src/likwid-pin.jl
+++ src/likwid-pin.jl
@@ -1,10 +1,10 @@
-function pinthreads(str::AbstractString)
-    cpuids = likwidpin_to_cpuids(str)
+function pinthreads(str::AbstractString; onebased = false)
+    cpuids = likwidpin_to_cpuids(str; onebased)
     @debug "CPU IDs" cpuids
     pinthreads(cpuids)
 end
 
-function likwidpin_to_cpuids(lpstr::AbstractString)
+function likwidpin_to_cpuids(lpstr::AbstractString; onebased = false)
     blocks = split(lpstr, '@')
     blocks_cpuids = Vector{Vector{Int}}(undef, length(blocks))
     for (i, block_str) in pairs(blocks)
@@ -18,7 +18,7 @@ function likwidpin_to_cpuids(lpstr::AbstractString)
                 # TODO
             else
                 @debug "likwid-pin: domain-based"
-                cpuids = _domainbased2cpuids(sections)
+                cpuids = _domainbased2cpuids(sections; onebased)
             end
         end
         blocks_cpuids[i] = cpuids
@@ -41,37 +41,42 @@ function _explicit2numbers(str)
     return numbers
 end
 
-function _domainbased2cpuids(sections)
+function _domainbased2cpuids(sections; onebased = false)
     @assert length(sections) > 1
     domain = sections[1]
-    if !is_valid_likwidpin_domain(domain)
-        throw(ArgumentError("Unknown domain \"$domain\". Valid domains are $(likwidpin_domains())."))
+    if !is_valid_likwidpin_domain(domain; onebased)
+        throw(ArgumentError("Unknown domain \"$domain\". Valid domains are $(likwidpin_domains(; onebased))."))
     end
     policy = sections[2]
     if policy == "scatter"
         numthreads = length(sections) > 2 ? parse(Int, sections[3]) : Threads.nthreads()
-        cpuids = _likwidpin_scatter_cpuids(domain, numthreads)
+        cpuids = _likwidpin_scatter_cpuids(domain, numthreads; onebased)
     else
         idcs = _explicit2numbers(policy) # logical indices, starting at 0(!)
-        cpuids = _likwidpin_domain_cpuids(domain, idcs)
+        cpuids = _likwidpin_domain_cpuids(domain, idcs; onebased)
     end
     return cpuids
 end
 
-function likwidpin_domains()
+function likwidpin_domains(; onebased = false)
     # ('N','S','D','M','C')
     domains = ["N", "S", "M"]
-    append!(domains, ["S$i" for i in 0:(nsockets() - 1)])
-    append!(domains, ["M$i" for i in 0:(nnuma() - 1)])
+    append!(domains, ["S$i" for i in (0 + onebased):(nsockets() - !onebased)])
+    append!(domains, ["M$i" for i in (0 + onebased):(nnuma() - !onebased)])
     # append!(domains, ["D$i" for i in TODO()])
     # append!(domains, ["C$i" for i in TODO()])
     return domains
 end
 
-is_valid_likwidpin_domain(domain::AbstractString) = domain in likwidpin_domains()
-is_valid_likwidpin_domain(domain::Symbol) = is_valid_likwidpin_domain(string(domain))
+function is_valid_likwidpin_domain(domain::AbstractString; kwargs...)
+    domain in likwidpin_domains(; kwargs...)
+end
+function is_valid_likwidpin_domain(domain::Symbol; kwargs...)
+    is_valid_likwidpin_domain(string(domain); kwargs...)
+end
 
-function _likwidpin_scatter_cpuids(domain, numthreads)
+function _likwidpin_scatter_cpuids(domain, numthreads; onebased = false)
+    offset = onebased ? 0 : 1
     if domain == "N"
         domain_cpuids = cpuids_per_node()
     elseif domain == "S"
@@ -80,10 +85,10 @@ function _likwidpin_scatter_cpuids(domain, numthreads)
         domain_cpuids = reduce(interweave, cpuids_per_numa())
     elseif startswith(domain, "S")
         socketid = parse(Int, domain[2:end])
-        domain_cpuids = cpuids_per_socket()[socketid + 1]
+        domain_cpuids = cpuids_per_socket()[socketid + offset]
     elseif startswith(domain, "M")
         numaid = parse(Int, domain[2:end])
-        domain_cpuids = cpuids_per_numa()[numaid + 1]
+        domain_cpuids = cpuids_per_numa()[numaid + offset]
     else
         error("Don't know how to handle domain \"$domain\" in domain:scatter[:numthreads] mode.")
     end
@@ -96,16 +101,17 @@ function _likwidpin_scatter_cpuids(domain, numthreads)
     return cpuids
 end
 
-function _likwidpin_domain_cpuids(domain, lp_idcs)
-    idcs = lp_idcs .+ 1 # zero- to one-based
+function _likwidpin_domain_cpuids(domain, lp_idcs; onebased = false)
+    offset = onebased ? 0 : 1
+    idcs = lp_idcs .+ offset
     if domain == "N"
         domain_cpuids = cpuids_per_node()
     elseif startswith(domain, "S") && length(domain) > 1
         socketid = parse(Int, domain[2:end])
-        domain_cpuids = cpuids_per_socket()[socketid + 1]
+        domain_cpuids = cpuids_per_socket()[socketid + offset]
     elseif startswith(domain, "M") && length(domain) > 1
         numaid = parse(Int, domain[2:end])
-        domain_cpuids = cpuids_per_numa()[numaid + 1]
+        domain_cpuids = cpuids_per_numa()[numaid + offset]
     else
         error("Don't know how to handle domain \"$domain\" in domain:explicit mode.")
     end
@@ -116,7 +122,7 @@ function _likwidpin_domain_cpuids(domain, lp_idcs)
         if checkbounds(Bool, domain_cpuids, idcs)
             @inbounds cpuids = domain_cpuids[idcs]
         else
-            error("Invalid logical CPU indices provided for domain \"domain\". Valid range: 0 to $(length(domain_cpuids)-1).")
+            error("Invalid logical CPU indices provided for domain \"domain\". Valid range: $(0 + onebased) to $(length(domain_cpuids)-offset).")
         end
     end
     return cpuids
