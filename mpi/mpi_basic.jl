7346114bdc58c8084047d16311e5c9718f5cb667
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2023-02-09 16:07:59 +0100

    docs: mpi start

--- mpi/mpi_basic.jl
+++ mpi/mpi_basic.jl
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
-#SBATCH -N 1
-#SBATCH -o sl_%j.out
+#SBATCH -N 2
+#SBATCH -n 4
+#SBATCH -o sl_mpi_basic_%j.out
 #SBATCH -A pc2-mitarbeiter
 #SBATCH -p all
 #SBATCH -t 00:02:00
 #=
-srun -n 4 julia --project -t 10 $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+srun -n 4 julia --project -t 1 $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
 exit
 # =#
 using MPI
@@ -15,8 +16,6 @@ MPI.Init()
 
 nranks = MPI.Comm_size(MPI.COMM_WORLD)
 rank = MPI.Comm_rank(MPI.COMM_WORLD)
-
-pinthreads_mpi(:sockets, rank, nranks)
 
 sleep(0.3 * rank)
 println("Rank $rank: ", getcpuids())
--- mpi/mpi_hybrid_explicit.jl
+++ mpi/mpi_hybrid_explicit.jl
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 #SBATCH -N 1
-#SBATCH -n 2
-#SBATCH --cpus-per-task=5
-#SBATCH --ntasks-per-socket=1
-#SBATCH -o sl_mpi_hybrid_mask%j.out
+#SBATCH -o sl_mpi_hybrid_explicit%j.out
 #SBATCH -A pc2-mitarbeiter
 #SBATCH -p all
 #SBATCH -t 00:02:00
 #=
-srun julia --project -t 5 $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+srun -n 4 julia --project -t 10 $(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
 exit
 # =#
 using MPI
@@ -16,9 +13,10 @@ using ThreadPinning
 
 MPI.Init()
 
+nranks = MPI.Comm_size(MPI.COMM_WORLD)
 rank = MPI.Comm_rank(MPI.COMM_WORLD)
 
-pinthreads(:affinitymask)
+pinthreads_mpi(:sockets, rank, nranks)
 
 sleep(0.3 * rank)
 println("Rank $rank: ", getcpuids())
