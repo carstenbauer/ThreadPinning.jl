1f846a2c1627b63d2c51d802adbccaa32ebbc279
Carsten Bauer <crstnbr@gmail.com>
2024-07-31 21:23:05 +0200

    fix

--- test/tests_mkl.jl
+++ test/tests_mkl.jl
@@ -4,12 +4,12 @@ using Test
 using MKL
 
 @testset "MKL utilities" begin
-    @test ThreadPinning.mkl_is_loaded()
-    @test ThreadPinning.mkl_get_dynamic() isa Integer
-    @test isnothing(ThreadPinning.mkl_set_dynamic(1))
-    @test ThreadPinning.mkl_get_dynamic() == 1
-    @test isnothing(ThreadPinning.mkl_set_dynamic(0))
-    @test ThreadPinning.mkl_get_dynamic() == 0
+    @test ThreadPinning.MKL.mkl_is_loaded()
+    @test ThreadPinning.MKL.mkl_get_dynamic() isa Integer
+    @test isnothing(ThreadPinning.MKL.mkl_set_dynamic(1))
+    @test ThreadPinning.MKL.mkl_get_dynamic() == 1
+    @test isnothing(ThreadPinning.MKL.mkl_set_dynamic(0))
+    @test ThreadPinning.MKL.mkl_get_dynamic() == 0
 end
 
 @testset "MKL threadinfo" begin
