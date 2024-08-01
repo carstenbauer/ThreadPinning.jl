13e5e95ed8aa51a1c2270165660f8d0f5d65328f
Carsten Bauer <crstnbr@gmail.com>
2024-08-01 07:26:23 +0200

    try fix docsbuild

--- docs/build_docs.jl
+++ docs/build_docs.jl
@@ -2,9 +2,9 @@ Threads.nthreads() > 1 ||
     error("Docs should be built with multiple Julia threads.")
 println("--- :julia: Instantiating project")
 using Pkg
-Pkg.activate("..")
+Pkg.activate(joinpath(@__DIR__, ".."))
 Pkg.instantiate()
-Pkg.activate(".")
+Pkg.activate(@__DIR__)
 Pkg.instantiate()
 push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 deleteat!(LOAD_PATH, 2)
